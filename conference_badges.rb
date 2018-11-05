a93e7c97aa8f492f6460b130c44219186d5808da
Jarrian McLean <jarrian.mclean@gmail.com>
2018-11-05 21:59:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+return  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+next_array = []
+array.each do |name|
+  next_array.push("Hello, my name is #{name}.")
+end
+return next_array
+end
+
+def assign_rooms()
+end
+
+def printer
